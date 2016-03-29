15d7ee0e1eebef598d5cdd9627e3a5b1e64923ba
Lopez <gustavo.lopez1@gmail.com>
2016-03-29 11:44:30 -0400

    First commit

---
+++ DeleteDupFiles.ps1
@@ -0,0 +1,19 @@
+﻿Function DeleteDupFiles($Directory,$Pattern1,$Pattern2){
+    Foreach($File in Get-ChildItem $Directory -Force -Recurse)
+    {
+        IF ((($File.BaseName -match $Pattern1) -or ($File.BaseName -match $Pattern2)) -and (-Not($_.PSIsContainer))) {
+            Write-Host "$("deleted: ")$($File.FullName)"
+            Write-Host ""
+            Remove-Item $File.FullName
+        }
+    }
+}
+
+#Example Usage
+#Be careful when using this commmand, if you don't know what you are doing 
+#you may inadvertently delete file you did not intend to delete
+#the command below will delete files where the filename contains the string "(1)" or the string "(2)"
+#which is a common filename pattern that occurs in Windows operating systems when files are duplicates
+#obviously this is a (perhaps too) simplistic approach
+#another approach might compare filesizes, etc.
+#deleteDupFiles  -Directory "C:\Users\username\Documents" -Pattern1 "\(1\)" -Pattern2 "\(2\)" 
